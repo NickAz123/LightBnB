432b3650188979fad4ccb02d793c6cb8d5fb780e
NickAzz <nick.azurin@gmail.com>
2021-05-27 21:56:10 +0000

    add property function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -180,14 +180,6 @@ const getAllProperties = (options, limit = 10) => {
     .catch((err) => {
       console.log("error", err.message)
     });;
-  // return pool
-  //   .query(
-  //   `SELECT *
-  //   FROM properties
-  //   LIMIT $1`, [limit]
-  //   )
-  //   .then((result) => { return result.rows })
-  //   .catch((err) => { console.log(err.message) });
 
 };
 exports.getAllProperties = getAllProperties;
@@ -199,9 +191,26 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let owner_id = parseInt(property.owner_id);
+  let title = property.title;
+  let description = property.description;
+  let thumbnail_photo_url = property.thumbnail_photo_url;
+  let cover_photo_url = property.cover_photo_url;
+  let cost_per_night = parseInt(property.cost_per_night*100);
+  let street = property.street;
+  let city = property.city;
+  let province = property.province;
+  let post_code = property.post_code;
+  let country = property.country;
+  let parking_spaces = parseInt(property.parking_spaces);
+  let number_of_bathrooms = parseInt(property.number_of_bathrooms);
+  let number_of_bedrooms = parseInt(property.number_of_bedrooms);
+
+
+  return pool.query(
+    `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;`, [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, true]
+  )
+  .then((res) => {return res.rows}).catch((err) => {console.log(err.message)});
 }
 exports.addProperty = addProperty;
